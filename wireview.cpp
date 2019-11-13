a8348440d8144b68205be7d45f924ed730e43b73
osboxes.org <jfgerulskis@wpi.edu>
2019-11-13 12:21:45 -0500

    Added additional printing, fixed null artihmatic warnings

--- wireview.cpp
+++ wireview.cpp
@@ -34,34 +34,34 @@ typedef struct udp_header{
     u_short crc;            // Checksum
 }udp_header;
 
-struct packetCaptureData {
+typedef struct computerInfo {
+	ip_address ipAddress;
+	std::string macAddress;
+}computerInfo;
+
+typedef struct packetCaptureData {
 	struct timeval startDateAndTime;
 	struct timeval endDateAndTime;
 	double duration; // must be computed at end
 	u_int total;
 	ip_address *uniqueSenders; 
 	ip_address *uniqueRecipients;
-	struct computerInfo *computers;
+	computerInfo *computers;
 	u_int *udpUniqueSourcePorts;
 	u_int *udpUniqueDestinationPorts;
 	u_int minPacketSize;
 	u_int maxPacketSize; 
 	u_int sumPacketSize;
 	u_int avgPacketSize; // must be computed at end
-};
+}packetCaptureData;
 
-struct packetCaptureData data;
+packetCaptureData data;
 
-struct computerInfo {
-	std::string ipAddress;
-	std::string macAddress;
-};
-
-void setStartDate(struct packetCaptureData &data, const struct pcap_pkthdr* packet) {
+void setStartDate(packetCaptureData &data, const struct pcap_pkthdr* packet) {
 	data.startDateAndTime = packet->ts;
 }
 
-void updateDuration(struct packetCaptureData &data){
+void updateDuration(packetCaptureData &data){
 	struct timeval diff = {
 		data.endDateAndTime.tv_sec - data.startDateAndTime.tv_sec,
 		data.endDateAndTime.tv_usec - data.startDateAndTime.tv_usec
@@ -69,34 +69,34 @@ void updateDuration(struct packetCaptureData &data){
 	data.duration = diff.tv_sec + diff.tv_usec / 1000000.0;
 }
 
-void updateEndTime(struct packetCaptureData &data, const struct pcap_pkthdr* packet) {
+void updateEndTime(packetCaptureData &data, const struct pcap_pkthdr* packet) {
 	data.endDateAndTime = packet->ts;
 }
 
-void updateTotal(struct packetCaptureData &data) {
+void updateTotal(packetCaptureData &data) {
 	data.total++;
 }
 
-void updateUniqueSenders(struct packetCaptureData &data, ip_address senderIP) {
+void updateUniqueSenders(packetCaptureData &data, ip_address senderIP) {
 	// TODO: implement
 }
 
-void updateUniqueRecipients(struct packetCaptureData &data, ip_address RecipientsIP) {
+void updateUniqueRecipients(packetCaptureData &data, ip_address RecipientsIP) {
 	// TODO: implement
 }
 
-void updateudpUniqueSourcePorts(struct packetCaptureData &data, u_short sourcePort) {
+void updateudpUniqueSourcePorts(packetCaptureData &data, u_short sourcePort) {
 	// TODO: implement
 }
 
-void updateudpUniqueDestinationPorts(struct packetCaptureData &data, u_short desitinationPort) {
+void updateudpUniqueDestinationPorts(packetCaptureData &data, u_short desitinationPort) {
 	// TODO: implement
 }
 
 /**
  * Source: https://www.winpcap.org/docs/docs_412/html/group__wpcap__tut6.html
  */
-void updateUniqueSendersAndReceiversIPandPorts(struct packetCaptureData &data, const u_char *packetData) {
+void updateUniqueSendersAndReceiversIPandPorts(packetCaptureData &data, const u_char *packetData) {
 	
 	ip_header *ih;
     udp_header *uh;
@@ -119,23 +119,23 @@ void updateUniqueSendersAndReceiversIPandPorts(struct packetCaptureData &data, c
 	updateudpUniqueDestinationPorts(data, dport);
 }
 
-void updataMinPacketSize(struct packetCaptureData &data, const struct pcap_pkthdr* packet) {
-	if (data.minPacketSize > packet->len || data.minPacketSize == NULL) {
+void updataMinPacketSize(packetCaptureData &data, const struct pcap_pkthdr* packet) {
+	if (data.minPacketSize > packet->len || !data.minPacketSize) {
 		data.minPacketSize = packet->len;
 	}
 }
 
-void updataMaxPacketSize(struct packetCaptureData &data, const struct pcap_pkthdr* packet) {
+void updataMaxPacketSize(packetCaptureData &data, const struct pcap_pkthdr* packet) {
 	if (data.maxPacketSize < packet->len) {
 		data.maxPacketSize = packet->len;
 	}
 }
 
-void updataSumPacketSize(struct packetCaptureData &data, const struct pcap_pkthdr* packet) {
+void updataSumPacketSize(packetCaptureData &data, const struct pcap_pkthdr* packet) {
 	data.sumPacketSize += packet->len;
 }
 
-void updateAvgPacketSize(struct packetCaptureData &data, const struct pcap_pkthdr* packet) {
+void updateAvgPacketSize(packetCaptureData &data, const struct pcap_pkthdr* packet) {
 	data.avgPacketSize = data.sumPacketSize / data.total;
 }
 
@@ -144,7 +144,7 @@ void updateAvgPacketSize(struct packetCaptureData &data, const struct pcap_pkthd
 * Callback function for pcap_loop()
 */
 void analyzePacket(u_char* a, const struct pcap_pkthdr* b, const u_char *c) {
-	if(data.startDateAndTime.tv_sec == NULL){
+	if(!data.startDateAndTime.tv_sec){
 		setStartDate(data, b);
 	}
 	updateEndTime(data, b);
@@ -155,9 +155,23 @@ void analyzePacket(u_char* a, const struct pcap_pkthdr* b, const u_char *c) {
 	updataSumPacketSize(data, b);
 	updateAvgPacketSize(data, b);
 	updateUniqueSendersAndReceiversIPandPorts(data, c);
+}
 
-	std::cout << "Total " << data.total << ", Min " << data.minPacketSize << ", Max " << data.maxPacketSize << ", Avg " << data.sumPacketSize / data.total << std::endl;
-	std::cout << data.duration << std::endl;
+void printPacketCaptureReport(packetCaptureData &data) {
+	std::cout << "___***PACKET_CAPTURE_REPORT***___" << std::endl << std::endl;
+	time_t time = (time_t)data.startDateAndTime.tv_sec;
+	std::cout << "Start date and time: \t" << ctime(&time);
+	std::cout << "Duration: \t\t" << data.duration << "s" << std::endl;
+	std::cout << "Total packets: \t\t" << data.total << std::endl;
+	// unique senders
+	// unique receivers
+	// list of machines participating in arp
+	// for UDP, unique source ports
+	// for UDP, unique destination ports
+	std::cout << "Average packet size: \t" << data.avgPacketSize << std::endl;
+	std::cout << "Minimum packet size: \t" << data.minPacketSize << std::endl;
+	std::cout << "Maximum packet size: \t" << data.maxPacketSize << std::endl;
+
 }
 
 /**
@@ -195,8 +209,7 @@ int main(int argc, char* argv[]) {
 	if (pcap_loop(file, MAX_PACKETS_TO_READ, analyzePacket, NULL) < 0) {
 		std::cout << "pcap_loop() failed: " << pcap_geterr(file) << std::endl;
 	}
-	time_t time = (time_t)data.startDateAndTime.tv_sec;
-	std::cout << ctime(&time) << std::endl;
-	std::cout << "Finished reading packet capture" << std::endl;
+	std::cout << "Finished reading packet capture\n" << std::endl;
+	printPacketCaptureReport(data);
 	pcap_close(file);
 }
