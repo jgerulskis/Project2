0c7820cbce0ba9fea24ee7c1208a45da17a73a77
osboxes.org <jfgerulskis@wpi.edu>
2019-11-12 22:14:39 -0500

    added packet ips and ports

--- wireview.cpp
+++ wireview.cpp
@@ -2,14 +2,45 @@
 #include <string.h>
 #include <pcap/pcap.h>
 #include <cstddef>
+#include <arpa/inet.h>
+
+typedef struct ip_address{
+    u_char byte1;
+    u_char byte2;
+    u_char byte3;
+    u_char byte4;
+}ip_address;
+
+/* IPv4 header */
+typedef struct ip_header{
+    u_char  ver_ihl;        // Version (4 bits) + Internet header length (4 bits)
+    u_char  tos;            // Type of service 
+    u_short tlen;           // Total length 
+    u_short identification; // Identification
+    u_short flags_fo;       // Flags (3 bits) + Fragment offset (13 bits)
+    u_char  ttl;            // Time to live
+    u_char  proto;          // Protocol
+    u_short crc;            // Header checksum
+    ip_address  saddr;      // Source address
+    ip_address  daddr;      // Destination address
+    u_int   op_pad;         // Option + Padding
+}ip_header;
+
+/* UDP header*/
+typedef struct udp_header{
+    u_short sport;          // Source port
+    u_short dport;          // Destination port
+    u_short len;            // Datagram length
+    u_short crc;            // Checksum
+}udp_header;
 
 struct packetCaptureData {
 	struct timeval startDateAndTime;
 	struct timeval endDateAndTime;
 	double duration; // must be computed at end
 	u_int total;
-	std::string *uniqueSenders; 
-	std::string *uniqueRecipients;
+	ip_address *uniqueSenders; 
+	ip_address *uniqueRecipients;
 	struct computerInfo *computers;
 	u_int *udpUniqueSourcePorts;
 	u_int *udpUniqueDestinationPorts;
@@ -38,8 +69,50 @@ void updateTotal(struct packetCaptureData &data) {
 	data.total++;
 }
 
+void updateUniqueSenders(struct packetCaptureData &data, ip_address senderIP) {
+	// TODO: implement
+}
+
+void updateUniqueRecipients(struct packetCaptureData &data, ip_address RecipientsIP) {
+	// TODO: implement
+}
+
+void updateudpUniqueSourcePorts(struct packetCaptureData &data, u_short sourcePort) {
+	// TODO: implement
+}
+
+void updateudpUniqueDestinationPorts(struct packetCaptureData &data, u_short desitinationPort) {
+	// TODO: implement
+}
+
+/**
+ * Source: https://www.winpcap.org/docs/docs_412/html/group__wpcap__tut6.html
+ */
+void updateUniqueSendersAndReceiversIPandPorts(struct packetCaptureData &data, const u_char *packetData) {
+	
+	ip_header *ih;
+    udp_header *uh;
+	u_int ip_len;
+    u_short sport,dport;
+	/* retireve the position of the ip header */
+    ih = (ip_header *) (packetData + 14); //length of ethernet header
+
+    /* retireve the position of the udp header */
+    ip_len = (ih->ver_ihl & 0xf) * 4;
+    uh = (udp_header *) ((u_char*)ih + ip_len);
+
+    /* convert from network byte order to host byte order */
+    sport = ntohs( uh->sport );
+    dport = ntohs( uh->dport );
+
+	updateUniqueSenders(data, ih->saddr);
+	updateUniqueRecipients(data, ih->daddr);
+	updateudpUniqueSourcePorts(data, sport);
+	updateudpUniqueDestinationPorts(data, dport);
+}
+
 void updataMinPacketSize(struct packetCaptureData &data, const struct pcap_pkthdr* packet) {
-	if (data.minPacketSize > packet->len) {
+	if (data.minPacketSize > packet->len || data.minPacketSize == NULL) {
 		data.minPacketSize = packet->len;
 	}
 }
@@ -54,6 +127,10 @@ void updataSumPacketSize(struct packetCaptureData &data, const struct pcap_pkthd
 	data.sumPacketSize += packet->len;
 }
 
+void updateAvgPacketSize(struct packetCaptureData &data, const struct pcap_pkthdr* packet) {
+	data.avgPacketSize = data.sumPacketSize / data.total;
+}
+
 
 /**
 * Callback function for pcap_loop()
@@ -64,7 +141,10 @@ void analyzePacket(u_char* a, const struct pcap_pkthdr* b, const u_char *c) {
 	updataMinPacketSize(data, b);
 	updataMaxPacketSize(data, b);
 	updataSumPacketSize(data, b);
-	std::cout << data.total << std::endl;
+	updateAvgPacketSize(data, b);
+	updateUniqueSendersAndReceiversIPandPorts(data, c);
+
+	std::cout << "Total " << data.total << ", Min " << data.minPacketSize << ", Max " << data.maxPacketSize << ", Avg " << data.sumPacketSize / data.total << std::endl;
 }
 
 /**
